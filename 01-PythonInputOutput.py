6f365eeb88722a833e76f9aa3d5bd54c64e31249
wacharathorniq <wacharathorniq@gmail.com>
2026-06-24 09:58:07 +0700

    Python variable test

---
+++ 01-PythonInputOutput.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+like = input("What do you like? ")
+
+print(name + " likes " + like + ".")
---
+++ 03-PythonVariables.py
@@ -0,0 +1,7 @@
+team1 = "Liverpool"
+team2 = "Chelsea"
+score1 = 4
+score2 = 3 
+
+print( team1 + " versus " + team2 )
+print( "Final score: " + str(score1) + " to " + str(score2) )
